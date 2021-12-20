9cf8f57295f3a18aae7c3b1f2b5bc94bc8a415b8
Leonardo <leo.barroso.silva@gmail.com>
2021-12-20 20:29:19 -0300

    Adiciona automaticamente permissões aos grupos criados por padrão

--- base/management/commands/carregar_dados_iniciais.py
+++ base/management/commands/carregar_dados_iniciais.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print('CARREGANDO DADOS INICIAIS')
-        Group.objects.get_or_create(name='Servidor')
-        Group.objects.get_or_create(name='Avaliador')
+        grupo_servidor = Group.objects.get_or_create(name='Servidor')
+        permission1 = Permission.objects.get(codename='add_vacancia')
+        permission2 = Permission.objects.get(codename='view_vacancia')
+        grupo_servidor[0].permissions.add(permission1)
+        grupo_servidor[0].permissions.add(permission2)
+        grupo_avaliador = Group.objects.get_or_create(name='Avaliador')
+        grupo_avaliador[0].permissions.add(permission1)
+        grupo_avaliador[0].permissions.add(permission2)
         print('FEITO!')
