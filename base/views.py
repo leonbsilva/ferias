46e440f0d125bcd9b96ec7cef7ef8f0a2e72935d
Leonardo <leo.barroso.silva@gmail.com>
2021-12-22 20:12:40 -0300

    Exibe mensagem de sucesso nos cadastros

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 # Create your views here.
 from base.forms import AdicionarUsuarioForm, AdicionarFeriasForm, RevisarFeriasForm, ListagemRevisaoFeriasForm, \
@@ -25,11 +26,12 @@ def listagem_ferias(request):
 
 
 def adicionar_ferias(request):
-    form = AdicionarFeriasForm(data=request.POST or None)
+    form = AdicionarFeriasForm(data=request.POST or None, user=request.user)
     if form.is_valid():
         ferias = form.save(commit=False)
         ferias.servidor = request.user
         ferias.save()
+        messages.success(request, 'Solicitação de férias cadastrada com sucesso')
         return HttpResponseRedirect('/base/listagem_ferias/')
     return render(request, 'adicionar_ferias.html', dict(form=form))
 
@@ -50,5 +52,6 @@ def revisar_ferias(request, vacation_id):
         ferias.situacao = form.cleaned_data['situacao']
         ferias.data_avaliacao = datetime.datetime.now()
         ferias.save()
+        messages.success(request, 'Solicitação de Férias avaliada com sucesso')
         return HttpResponseRedirect('/base/listagem_revisao_ferias')
     return render(request, 'revisar_ferias.html', dict(form=form))
